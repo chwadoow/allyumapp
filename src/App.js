1d731b139de32e0bac643050863efc340aedc6d2
moses ngige <moses32ngigz@gmail.com>
2022-07-30 17:45:44 +0300

    About.js done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav,  Container } from 'react-bootstrap'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './components/About';
+import  Contact  from './components/Contact';
+import Home from './components/Home';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="dark" variant='dark' expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">AllYums</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+             
+            </Nav>
+           
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
+
+    <div>
+      <Routes>
+
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/" element={<Home/>}/>
+
+      </Routes>
+
+    </div>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='about'>
+    <h1>About</h1>
+
+    <strong>This recipe app was created to help one keep get recipes to try out.
+      It also give one an option to add on new recipes to the database enabling 
+      users to share their recipes with other user of the website.
+</strong>
+<strong>Development for the website is still in progress.For input you can use the Contact details to contact the developer</strong>
+        
+
+  
+
+
+
+
+   
+
+  </div>
+  )
+}
+
+export default About
