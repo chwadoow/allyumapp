9a1b621eb72e8a2f5a1f979ee1ca29ee6ba0baf4
moses ngige <moses32ngigz@gmail.com>
2022-07-30 17:45:29 +0300

    Added Contacts.js

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+export default function Pricing() {
+    return <div className='contacts'>
+      <h1>Contacts</h1>
+
+<h1>Creator Contacts</h1>
+      <p>Email: moses32ngigz@gmail.com.</p>
+      <p>PO BOX : 40802-72</p>
+      <p>Cell Number:07xxxxxxxx</p>
+
+      <h2>For any assistance use the above to contact developer</h2>
+    </div>
+
+
+  }
