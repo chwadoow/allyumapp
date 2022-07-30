f0fb0874c2aea1da2ee27cb5e93238fd7ee814dc
moses ngige <moses32ngigz@gmail.com>
2022-07-30 17:45:12 +0300

    Added FirstData.js

---
+++ src/components/FirstData.js
@@ -0,0 +1,40 @@
+import {useState,useEffect} from 'react'
+import SecondData from './SecondData'
+import FormSub from './FormSub'
+function FirstData() {
+  const[data,setData]=useState([])
+    
+  useEffect(()=>{
+   fetch('http://localhost:3000/meal')
+   .then((res)=>res.json())
+   .then((res)=>setData(res))
+
+
+  },[])
+  const secondBreak =  data.map((element)=>(
+    <SecondData
+    key={element['id']}
+    name={element['name']}
+    ingredients={element['ingredients']}
+    link={element['link']}
+    youtube={element['strYoutube']}
+    steps={element['steps']}
+    
+    
+    />
+  )
+   
+  )
+function addToRecipes(res){
+  const newData=[...data,res]
+  setData(newData)
+}
+return (
+    <div  >
+<FormSub addToRecipes={addToRecipes}/>
+{secondBreak}
+    </div>
+  )
+}
+
+export default FirstData
