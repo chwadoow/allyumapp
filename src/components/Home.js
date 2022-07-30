e9201e618dcd71efa61765b3233a48bca109d093
moses ngige <moses32ngigz@gmail.com>
2022-07-30 17:44:32 +0300

    Added home.js

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react"
+import FirstData from "./FirstData"
+
+export default function Home() {
+   
+  
+   
+ 
+   
+    return (
+<div className="home" style={{background:'orange'}}>
+<h1>Home</h1>
+
+<div className="upper" style={{background:'orange'}}>
+<FirstData/>
+</div>
+
+
+
+</div>
+)}
