b1b05ad81feb0c19e23d33c6f09a5797bd9a62e6
moses ngige <moses32ngigz@gmail.com>
2022-07-31 10:35:12 +0300

    changes to about.js font

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,12 @@ function About() {
     <div className='about'>
     <h1>About</h1>
 
-    <h3>Allyum is a recipe app that is meant to give it's users a wide variety of recipes that users can cook.
+    <h5>Allyum is a recipe app that is meant to give it's users a wide variety of recipes that users can cook.
        It gives not only step by step guide but also includes videos to give clearer information on how each dish is prepared.
         It also offers its users a chance to share there recipes with other users across the world.
 
-</h3>
-<h2>Development for the website is still in progress.For input you can use the Contact details to contact the developer</h2>
+</h5>
+<strong>Development for the website is still in progress.For input you can use the Contact details to contact the developer</strong>
         
 
   
